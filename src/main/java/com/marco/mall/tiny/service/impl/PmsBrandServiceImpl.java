9444a242dcf78a060ece843543b2cf3a1b020005
yaozeyu3 <yaozeyu3@jd.com>
2022-04-02 09:58:43 +0800

    实现Service接口

---
+++ src/main/java/com/marco/mall/tiny/service/impl/PmsBrandServiceImpl.java
@@ -0,0 +1,41 @@
+package com.marco.mall.tiny.service.impl;
+
+import com.marco.mall.tiny.dto.PmsBrand;
+import com.marco.mall.tiny.service.PmsBrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class PmsBrandServiceImpl implements PmsBrandService {
+    @Autowired
+    private PmsBrandMapper brandMapper;
+    @Override
+    public List<PmsBrand> listAllBrand() {
+        return null;
+    }
+
+    @Override
+    public int createBrand(PmsBrand brand) {
+        return 0;
+    }
+
+    @Override
+    public int updateBrand(Long id, PmsBrand brand) {
+        return 0;
+    }
+
+    @Override
+    public int deleteBrand(Long id) {
+        return 0;
+    }
+
+    @Override
+    public List<PmsBrand> listBrand(int pageNum, int pageSize) {
+        return null;
+    }
+
+    @Override
+    public PmsBrand getBrand(Long id) {
+        return null;
+    }
+}
